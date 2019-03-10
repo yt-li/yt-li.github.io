d334de3eea5a3fa33458c2c6478e6b867a5b9295
yt-li <30373846+yt-li@users.noreply.github.com>
2019-03-10 14:36:13 +0000

    Create biography.m

---
+++ biography.m
@@ -0,0 +1,26 @@
+<br />
+
+## Biography
+
+<br />
+
+### Education
+
+2011 - 2015, BEng, Huazhong University of Science and Technology, China
+2014 - 2015, BEng, University of Birmingham, UK
+2015 - 2016, Msc, Imperial College London, UK
+2017 - ...., PhD, Imperial College London, UK
+
+### Research
+
+**Yitong Li**`s current research includes control techniques and power electronic converters for ac and dc distribution networks and power systems. Applications include medium- or low-voltage distribution grids, microgrids, distributed generations, sustainable and renewable energy systems, electric vehicles, etc. He also has general interests in advanced algorithms, control structures, and analysis tools for future low-carbon smart grids.
+
+<br />
+
+### Useful Links
+
+[Publication](https://yt-li.github.io/publication) | [Google Scholar](https://scholar.google.co.uk/citations?hl=en&user=b3tutrQAAAAJ&view_op=list_works&sortby=pubdate)
+
+<br />
+
+### [back](https://yt-li.github.io/)
